8035a840f7387ce535a33d5643b096b192449d65
Juan Baez <jbaez001@gmail.com>
2023-10-29 01:28:42 -0600

    refactor: moving subcomponents to their proper place

--- src/components/Pomodoro/Pomodoro.tsx
+++ src/components/Pomodoro/Pomodoro.tsx
@@ -15,7 +15,7 @@ limitations under the License.
 */
 
 import { IPomodoro } from '../../interfaces/pomodoros';
-import { PomodoroCard } from '../PomodoroCard';
+import { PomodoroCard } from './PomodoroCard';
 import { usePomodoro } from './usePomodoro';
 
 export const Pomodoro = (props: IPomodoro) => {
